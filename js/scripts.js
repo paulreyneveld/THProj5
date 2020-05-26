134b9f319b25ec52ce6c7e99d824af31002dca5c
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-05-26 14:54:08 -0700

    Working on modal window

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,39 @@
-// Start by obtaining the data from the API. 
-// I'm borrowing this code from the TH course on the same topic. 
-
 const galleryDiv = document.querySelector('#gallery');
 
+// async to retrieve Promise when ready
 async function fetchData(url) {
 	const response = await fetch(url);
 	const data = await response.json();
-	dataPusher(data.results);
+	const userList = data.results;
+	// passes data to create main html
+	generateMainHTML(userList);
+
+	// The following function should compare the userList against the user interaction and display data
+	// based on the user's click. 
+	// generateModalHTML(userList);
 }
+
+// calls api for data promise object
 fetchData("https://randomuser.me/api/?results=12&nat=us");
 
-function dataPusher(userList) {
-	userList.forEach( user => {
-		generateMainHTML(user.picture.medium, user.name.first, 
-				user.name.last, user.email, 
-				user.location.city, user.location.state);
+function generateMainHTML(userList) {
+	userList.forEach(user => {
+		const cardDiv = document.createElement('div');
+		const imgContDiv = document.createElement('div');
+		const infoContDiv = document.createElement('div');
+
+		cardDiv.className = 'card';
+		imgContDiv.className = 'card-img-container';
+		infoContDiv.className = 'card-info-container';
+		galleryDiv.appendChild(cardDiv);
+		cardDiv.appendChild(imgContDiv);
+		cardDiv.appendChild(infoContDiv);
+
+		imgContDiv.innerHTML = `<img class="card-img" src="${user.picture.medium}" alt="profile picture">`;
+		infoContDiv.innerHTML = `<h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+						<p class="card-text">${user.email}</p>
+						<p class="card-text cap">${user.location.city}, ${user.location.state}</p>`;
 	});
 }
 
-// So the fact of the matter is that I don't need my dataPusher function. I should just
-// push the data into the function and then use a forEach in the function to achieve the same effect.
-// I should refactor this code to embrace that idea before setting up the modal window as 
-// I want it to work in basically the same fashion. 
-
-function generateMainHTML(userImgURL, firstName, lastName, email, city, state) {
-	const cardDiv = document.createElement('div');
-	const imgContDiv = document.createElement('div');
-	const infoContDiv = document.createElement('div');
-
-	cardDiv.className = 'card';
-	imgContDiv.className = 'card-img-container';
-	infoContDiv.className = 'card-info-container';
-	galleryDiv.appendChild(cardDiv);
-	cardDiv.appendChild(imgContDiv);
-	cardDiv.appendChild(infoContDiv);
-
-	imgContDiv.innerHTML = `<img class="card-img" src="${userImgURL}" alt="profile picture">`;
-	infoContDiv.innerHTML = `<h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-					<p class="card-text">${email}</p>
-					<p class="card-text cap">${city}, ${state}</p>`;
-}
-
-function generateModalHTML(userImgURL, firstName, lastName, email, city, cell, address, birthday) {
-	
-}
+// generateModalHTML(userList) {}
