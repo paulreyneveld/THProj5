ebf2af8e2af6f7f90fdedb7d08130457bc51e4d4
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-05-25 19:52:00 -0700

    Add the functionality required to obtain the data from the API

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,46 @@
 // Start by obtaining the data from the API. 
 // I'm borrowing this code from the TH course on the same topic. 
-function fetchData(url) {
-	return fetch(url)
-		.then(response => response.json())
-} 
 
-const data = fetchData('https://randomuser.me/api/?results=12')
-	// The following line allows for access to individual pieces of data from the returned object. 
-	//.then(data => console.log(data.info, data.results, data.results[3].email) );
-	.then(data => {
-		console.log(data.results[3].email);
-		console.log(data.results);
-		console.log(typeof data);
-		const users = data.results.map(user => {
-			return user.email;
-		});
-		console.log(users)
+const galleryDiv = document.querySelector('#gallery');
+
+async function fetchData(url) {
+	const response = await fetch(url);
+	const data = await response.json();
+	dataPusher(data.results);
+}
+fetchData("https://randomuser.me/api/?results=12&nat=us");
+
+function dataPusher(userList) {
+	userList.forEach( user => {
+		generateMainHTML(user.picture.medium, user.name.first, 
+				user.name.last, user.email, 
+				user.location.city, user.location.state);
 	});
+}
 
-	// Okay, I have some things going for me right now, but I need to figure out how to map the incoming data to
-	// individual objects with only the info I want. I also need to do some error checking with regard to the 
-	// initial retrieval of the data. 
+// So the fact of the matter is that I don't need my dataPusher function. I should just
+// push the data into the function and then use a forEach in the function to achieve the same effect.
+// I should refactor this code to embrace that idea before setting up the modal window as 
+// I want it to work in basically the same fashion. 
 
-console.log(data);
-// Okay, so I have the data coming in. I'm going to want to map it somehow, but I'm 
-// not yet clear on exactly how I'm going to do that. 
+function generateMainHTML(userImgURL, firstName, lastName, email, city, state) {
+	const cardDiv = document.createElement('div');
+	const imgContDiv = document.createElement('div');
+	const infoContDiv = document.createElement('div');
+
+	cardDiv.className = 'card';
+	imgContDiv.className = 'card-img-container';
+	infoContDiv.className = 'card-info-container';
+	galleryDiv.appendChild(cardDiv);
+	cardDiv.appendChild(imgContDiv);
+	cardDiv.appendChild(infoContDiv);
+
+	imgContDiv.innerHTML = `<img class="card-img" src="${userImgURL}" alt="profile picture">`;
+	infoContDiv.innerHTML = `<h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+					<p class="card-text">${email}</p>
+					<p class="card-text cap">${city}, ${state}</p>`;
+}
+
+function generateModalHTML(userImgURL, firstName, lastName, email, city, cell, address, birthday) {
+	
+}
